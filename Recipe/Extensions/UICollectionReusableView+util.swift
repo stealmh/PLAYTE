a7a24185c770b9bf5655da80a9802588c256a14d
김민호 <66459715+stealmh@users.noreply.github.com>
2023-07-04 17:19:19 +0900

    feat: #4 Cell Extension 추가

---
+++ Recipe/Extensions/UICollectionReusableView+util.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionReusableView+util.swift
+//  Recipe
+//
+//  Created by KindSoft on 2023/07/04.
+//
+
+import UIKit
+
+//UICollectionViewCell + UICollectionReusableView
+extension UICollectionReusableView {
+    static var reuseIdentifier: String {
+        return String(describing: Self.self)
+    }
+}
--- Recipe/Extensions/UIViewController+util.swift
+++ Recipe/Extensions/UIViewController+util.swift
@@ -5,4 +5,59 @@
 //  Created by KindSoft on 2023/07/03.
 //
 
-import Foundation
+import UIKit
+import SwiftUI
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+    func toPreview() -> some View {
+            Preview(viewController: self)
+    }
+    
+    func isScrolledToBottom(_ myPosition: CGPoint, _ tableView: UITableView) -> Bool {
+        if myPosition.y > tableView.contentSize.height - 100 - tableView.bounds.size.height {
+            return true
+        }
+        return false
+    }
+    
+    /// For Toast Alert
+    func showToast(message : String,
+                   textColor: UIColor,
+                   backgroundColor: UIColor,
+                   width:CGFloat,
+                   height: CGFloat) {
+            let v = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - (width/2), y: self.view.frame.size.height-100, width: width, height: height))
+        v.backgroundColor = backgroundColor.withAlphaComponent(0.6)
+            v.textColor = textColor
+            v.font = UIFont.systemFont(ofSize: 14)
+            v.textAlignment = .center
+            v.text = message
+            v.alpha = 1.0
+            v.layer.cornerRadius = 10
+            v.clipsToBounds = true
+            self.view.addSubview(v)
+        UIView.animate(withDuration: 1, delay: 1, options: .curveEaseOut, animations: {
+                 v.alpha = 0.0
+            //아래쪽으로 사라지게 하기 위함
+            let scale = CGAffineTransform(translationX: 0, y: 200)
+            v.transform = scale
+            }, completion: {(isCompleted) in
+                v.removeFromSuperview()
+            })
+        }
+}
+#endif
+
+
