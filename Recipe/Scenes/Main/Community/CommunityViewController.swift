204e948bf2c67ef41123fb5f1ff54917ef58d72a
김민호 <66459715+stealmh@users.noreply.github.com>
2023-07-07 15:03:03 +0900

    feat: #8 커뮤니티 탭바 추가

---
+++ Recipe/Scenes/Main/Community/CommunityViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CommunityViewController.swift
+//  Recipe
+//
+//  Created by KindSoft on 2023/07/07.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+final class CommunityViewController: BaseViewController {
+    
+    var didSendEventClosure: ((CommunityViewController.Event) -> Void)?
+    var disposeBag = DisposeBag()
+    
+    private let button: UIButton = {
+        let v = UIButton()
+        v.setTitle("로그아웃", for: .normal)
+        v.setTitleColor(.black, for: .normal)
+        return v
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(button)
+        
+        button.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        button.rx.tap
+            .subscribe(onNext: { _ in
+                self.didSendEventClosure?(.go)
+            }).disposed(by: disposeBag)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        disposeBag = DisposeBag()
+    }
+    
+    enum Event {
+        case go
+    }
+}
+
