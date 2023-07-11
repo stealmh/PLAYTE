e4a33c68664ed99fedf211bd86ed0c0d8e1877de
김민호 <66459715+stealmh@users.noreply.github.com>
2023-07-11 18:17:56 +0900

    feat: #13 레시피 화면 구성

--- Recipe/Scenes/Main/Recipe/RecipeViewController.swift
+++ Recipe/Scenes/Main/Recipe/RecipeViewController.swift
@@ -13,14 +13,36 @@ import SnapKit
 final class RecipeViewController: BaseViewController {
 
     var disposeBag = DisposeBag()
+    private let recipeView = RecipeView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(recipeView)
+        recipeView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.left.right.bottom.equalToSuperview()
         }
+        configureNavigationTabBar()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         disposeBag = DisposeBag()
     }
+}
+
+
+extension RecipeViewController {
+    func configureNavigationTabBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .add)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.menuButton(imageName: "recipe_logo", size: CGSize(width: 64, height: 14))
+        navigationController?.navigationBar.barTintColor = .white
+        let v = UILabel()
+        v.text = "레시피"
+        navigationItem.titleView = v
+
+        navigationItem.rightBarButtonItem?.rx.tap
+            .subscribe(onNext: { _ in
+            }).disposed(by: disposeBag)
     }
+
 }
