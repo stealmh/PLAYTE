a858ffe89517d3c0f98392c5eb3d004f9277d945
김민호 <66459715+stealmh@users.noreply.github.com>
2023-07-13 15:08:35 +0900

    feat: #13 레시피 화면 구성

--- Recipe/Scenes/Main/Recipe/RecipeViewController.swift
+++ Recipe/Scenes/Main/Recipe/RecipeViewController.swift
@@ -11,18 +11,23 @@ import RxCocoa
 import SnapKit
 
 final class RecipeViewController: BaseViewController {
-
-    var disposeBag = DisposeBag()
+    
+    private var disposeBag = DisposeBag()
     private let recipeView = RecipeView()
+    private let floatingButton = DefaultFloatingButton()
+    var didSendEventClosure: ((RecipeViewController.Event) -> Void)?
+    
+    enum Event {
+        case showFloatingView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(recipeView)
-        recipeView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.left.right.bottom.equalToSuperview()
-        }
+        addView()
+        configureLayout()
         configureNavigationTabBar()
+        bind()
+        floatingButton.delegate = self
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -30,19 +35,47 @@ final class RecipeViewController: BaseViewController {
     }
 }
 
-
+//MARK: - Method(Normal)
 extension RecipeViewController {
-    func configureNavigationTabBar() {
+    private func addView() {
+        view.addSubViews(recipeView, floatingButton)
+    }
+    
+    private func configureLayout() {
+        recipeView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.left.right.bottom.equalToSuperview()
+        }
+        floatingButton.snp.makeConstraints {
+            $0.width.height.equalTo(62)
+            $0.bottom.equalToSuperview().inset(20)
+            $0.right.equalToSuperview().inset(20)
+        }
+    }
+    
+    private func configureNavigationTabBar() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .add)
         navigationItem.leftBarButtonItem = UIBarButtonItem.menuButton(imageName: "recipe_logo", size: CGSize(width: 64, height: 14))
         navigationController?.navigationBar.barTintColor = .white
         let v = UILabel()
         v.text = "레시피"
         navigationItem.titleView = v
+    }
+}
 
+//MARK: - Method(Rx Bind)
+extension RecipeViewController {
+    private func bind() {
         navigationItem.rightBarButtonItem?.rx.tap
             .subscribe(onNext: { _ in
             }).disposed(by: disposeBag)
     }
-
+}
+//MARK: - Method(플로팅 버튼 Delegate)
+extension RecipeViewController: FloatingButtonDelegate {
+    func floatingButtonTapped() {
+        didSendEventClosure?(.showFloatingView)
+        ///Todo: 어케분리하지
+        self.tabBarController?.tabBar.layer.zPosition = -1
+    }
 }
