b6688fa296b36c48489a55cdccfa35f58459f905
stealmh <kmh922@naver.com>
2023-07-14 02:00:32 +0900

    feat: #15 레시피 생성 뷰컨 생성

---
+++ Recipe/Scenes/Main/Recipe/CreateRecipeViewController.swift
@@ -0,0 +1,68 @@
+//
+//  CreateRecipeViewController.swift
+//  Recipe
+//
+//  Created by 김민호 on 2023/07/14.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+
+final class CreateRecipeViewController: BaseViewController {
+    
+    private let createRecipeView = CreateRecipeView()
+    private var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addView()
+        configureLayout()
+        bind()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .add)
+        navigationItem.title = "나의 레시피 작성"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        disposeBag = DisposeBag()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        centerTitle()
+    }
+
+}
+//MARK: - Method(Normal)
+extension CreateRecipeViewController {
+    func addView() {
+        view.addSubViews(createRecipeView)
+    }
+    
+    func configureLayout() {
+        createRecipeView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.left.right.bottom.equalToSuperview()
+        }
+    }
+}
+//MARK: - Method(Rx bind)
+extension CreateRecipeViewController {
+    func bind() {
+        
+    }
+}
+
+//MARK: - Preview
+import SwiftUI
+@available(iOS 13.0, *)
+struct CreateRecipeViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        UINavigationController(rootViewController: CreateRecipeViewController())
+            .toPreview()
+    }
+}
