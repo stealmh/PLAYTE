fcb8f2417ba1b37e6073814e0a43a2f84b5be1d1
stealmh <kmh922@naver.com>
2023-08-02 01:52:16 +0900

    feat: #20 숏폼 코디네이터 추가

--- Recipe/Scenes/Main/Tab/TabBarStyle.swift
+++ Recipe/Scenes/Main/Tab/TabBarStyle.swift
@@ -8,17 +8,20 @@
 import UIKit
 
 enum TabBarPage {
-    case community
-    case myPage
+    case shortForm
     case recipe
+    case upload
+    case myPage
 
     init?(index: Int) {
         switch index {
         case 0:
-            self = .recipe
+            self = .shortForm
         case 1:
-            self = .community
+            self = .recipe
         case 2:
+            self = .upload
+        case 3:
             self = .myPage
         default:
             return nil
@@ -27,10 +30,12 @@ enum TabBarPage {
     
     func pageTitleValue() -> String {
         switch self {
+        case .shortForm:
+            return "숏폼"
         case .recipe:
             return "레시피"
-        case .community:
-            return "커뮤니티"
+        case .upload:
+            return "업로드"
         case .myPage:
             return "마이페이지"
         }
@@ -39,36 +44,42 @@ enum TabBarPage {
     //tag
     func pageOrderNumber() -> Int {
         switch self {
-        case .recipe:
+        case .shortForm:
             return 0
-        case .community:
+        case .recipe:
             return 1
-        case .myPage:
+        case .upload:
             return 2
+        case .myPage:
+            return 3
         }
     }
 
     // Add tab icon value
     func tabIcon() -> UIImage {
         switch self {
+        case .shortForm:
+            return UIImage(named: "shortform")!
         case .recipe:
-            return UIImage(systemName: "house")!
-        case .community:
-            return UIImage(systemName: "house")!
+            return UIImage(named: "recipe")!
+        case .upload:
+            return UIImage(named: "upload")!
         case .myPage:
-            return UIImage(systemName: "plus")!
+            return UIImage(named: "mypage")!
         }
     }
     
     // Add tab icon selected / deselected color
     func selectedImage() -> UIImage {
         switch self {
+        case .shortForm:
+            return UIImage(named: "shortform.fill")!
         case .recipe:
-            return UIImage(systemName: "house.fill")!
-        case .community:
-            return UIImage(systemName: "house.fill")!
+            return UIImage(named: "recipe.fill")!
+        case .upload:
+            return UIImage(named: "upload.fill")!
         case .myPage:
-            return UIImage(systemName: "plus.circle.fill")!
+            return UIImage(named: "mypage.fill")!
         }
     }
     // etc
--- Recipe/Scenes/Main/Tab/TabCoordinator.swift
+++ Recipe/Scenes/Main/Tab/TabCoordinator.swift
@@ -20,31 +20,40 @@ protocol TabCoordinatorProtocol: Coordinator {
 final class TabCoordinator: NSObject, TabCoordinatorProtocol, CoordinatorFinishDelegate {
     
     weak var finishDelegate: CoordinatorFinishDelegate?
-        
+    
     var childCoordinators: [Coordinator] = []
-
+    
     var navigationController: UINavigationController
     
     var tabBarController: UITabBarController
-
+    
     var type: CoordinatorType { .tab }
     
     required init(_ navigationController: UINavigationController) {
         self.navigationController = navigationController
         self.tabBarController = .init()
     }
-
+    
     func start() {
         // Let's define which pages do we want to add into tab bar
-        let pages: [TabBarPage] = [.myPage, .community, .recipe]
+        let pages: [TabBarPage] = [.shortForm, .recipe, .upload, .myPage]
             .sorted(by: { $0.pageOrderNumber() < $1.pageOrderNumber() })
         
         // Initialization of ViewControllers or these pages
         let controllers: [UINavigationController] = pages.map({ getTabController($0) })
-//        print(controllers)
+        //        print(controllers)
         
         prepareTabBarController(withTabControllers: controllers)
-    
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .white
+            tabBarController.tabBar.standardAppearance = appearance
+            tabBarController.tabBar.scrollEdgeAppearance = tabBarController.tabBar.standardAppearance
+        } else {
+            tabBarController.tabBar.barTintColor = .white
+        }
+        
     }
     
     deinit {
@@ -55,42 +64,51 @@ final class TabCoordinator: NSObject, TabCoordinatorProtocol, CoordinatorFinishD
         /// Set delegate for UITabBarController
         tabBarController.delegate = self
         /// Assign page's controllers
-//        tabBarController.setViewControllers(tabControllers, animated: true)
+        //        tabBarController.setViewControllers(tabControllers, animated: true)
         /// Let set index
         tabBarController.selectedIndex = TabBarPage.recipe.pageOrderNumber()
         /// Styling
         tabBarController.tabBar.isTranslucent = false
         
         /// In this step, we attach tabBarController to navigation controller associated with this coordanator
-//        navigationController.viewControllers = [tabBarController]
+        //        navigationController.viewControllers = [tabBarController]
         tabBarController.viewControllers = tabControllers
-
+        changeRadius(cornerRadius: 13)
+        tabBarController.viewDidLayoutSubviews()
         
     }
-      
+    
     private func getTabController(_ page: TabBarPage) -> UINavigationController {
         let navController = UINavigationController()
-//        navController.setNavigationBarHidden(false, animated: false)
-
+        //        navController.setNavigationBarHidden(false, animated: false)
+        
         navController.tabBarItem = UITabBarItem.init(title: page.pageTitleValue(),
                                                      image: page.tabIcon(),
                                                      tag: page.pageOrderNumber())
         navController.tabBarItem.selectedImage = page.selectedImage()
-
         tabBarController.tabBar.backgroundColor = .white
+        tabBarController.tabBar.tintColor = .mainColor
+        tabBarController.tabBar.unselectedItemTintColor = .grayScale5
+        
         switch page {
+        case .shortForm:
+            let readyCoordinator = ShortFormCoordinator(navController)
+            readyCoordinator.finishDelegate = self
+            childCoordinators.append(readyCoordinator)
+            readyCoordinator.start()
+            
         case .recipe:
             let readyCoordinator = RecipeCoordinator(navController)
             readyCoordinator.finishDelegate = self
             childCoordinators.append(readyCoordinator)
             readyCoordinator.start()
             
-        case .community:
+        case .upload:
             let readyCoordinator = CommunityCoordinator(navController)
             readyCoordinator.finishDelegate = self
             childCoordinators.append(readyCoordinator)
             readyCoordinator.start()
-
+            
         case .myPage:
             let readyCoordinator = MypageCoordinator(navController)
             readyCoordinator.finishDelegate = self
@@ -102,7 +120,7 @@ final class TabCoordinator: NSObject, TabCoordinatorProtocol, CoordinatorFinishD
     }
     
     func currentPage() -> TabBarPage? { TabBarPage.init(index: tabBarController.selectedIndex) }
-
+    
     func selectPage(_ page: TabBarPage) {
         tabBarController.selectedIndex = page.pageOrderNumber()
     }
@@ -115,6 +133,13 @@ final class TabCoordinator: NSObject, TabCoordinatorProtocol, CoordinatorFinishD
     
     func coordinatorDidFinish(childCoordinator: Coordinator) {
         finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+    }
+    
+    private func changeRadius(cornerRadius: CGFloat) {
+        tabBarController.tabBar.layer.masksToBounds = true
+        tabBarController.tabBar.isTranslucent = true
+        tabBarController.tabBar.layer.cornerRadius = cornerRadius
+//        tabBarController.tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
 }
 
