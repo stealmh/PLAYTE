033c7040ca9371a0636448e3077015ed6138ebad
stealmh <kmh922@naver.com>
2023-08-21 00:36:56 +0900

    feat: Date 관련 String Extension 추가

--- Recipe/Extensions/String+util.swift
+++ Recipe/Extensions/String+util.swift
@@ -18,4 +18,53 @@ extension String {
         
         return isValid
     }
+    
+    func timeAgo() -> String? {
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        
+        guard let date = dateFormatter.date(from: self) else {
+            return nil
+        }
+
+        let currentDate = Date()
+        let calendar = Calendar.current
+        let koreanTimeZone = TimeZone(identifier: "Asia/Seoul")!
+        let components = calendar.dateComponents(in: koreanTimeZone, from: date)
+        let currentComponents = calendar.dateComponents(in: koreanTimeZone, from: currentDate)
+        let diffComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: components, to: currentComponents)
+
+        if let year = diffComponents.year, year > 0 {
+            return "\(year)년 전"
+        }
+        
+        if let month = diffComponents.month, month > 0 {
+            return "\(month)개월 전"
+        }
+        
+        if let day = diffComponents.day, day > 0 {
+            return "\(day)일 전"
+        }
+        
+        if let hour = diffComponents.hour, hour > 0 {
+            return "\(hour)시간 전"
+        }
+        
+        if let minute = diffComponents.minute, minute > 0 {
+            return "\(minute)분 전"
+        }
+        
+        return "방금 전"
+    }
+    
+    func toDateFormatted() -> String? {
+        let formatter = DateFormatter()
+
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS"
+        guard let date = formatter.date(from: self) else { return nil }
+
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: date)
+    }
 }
