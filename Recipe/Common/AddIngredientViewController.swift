601b336012679844d086287fa4f8bd164268924d
stealmh <kmh922@naver.com>
2023-08-21 15:11:10 +0900

    refactor: 기본 재료구성 파일 추가에 따른 뷰컨 로직 수정

--- Recipe/Common/AddIngredientViewController.swift
+++ Recipe/Common/AddIngredientViewController.swift
@@ -9,58 +9,76 @@ import UIKit
 import SnapKit
 
 class AddIngredientViewController: BaseViewController {
-    /*
-     gram
-     개수
-     ml
-     T
-     */
     var didSendEventClosure: ((AddIngredientViewController.Event) -> Void)?
-    var item: IngredientInfo?
+    var item: IngredientInfo
     enum Event {
         case cancelButtonTapped
         case okButtonTapped
     }
     
-    
     private let gramView = AddIngredientCountView()
+    private let defaultView = AddIngredientDefaultView()
+    
     var forTag: String = ""
+    
+    init(item: IngredientInfo) {
+        self.item = item
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(gramView)
-        view.backgroundColor = .grayScale6?.withAlphaComponent(0.5)
-        gramView.delegate = self
         
-        gramView.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.width.equalTo(327)
-            $0.height.equalTo(205)
+        if item.ingredient_type == "PIECES" {
+            view.addSubview(gramView)
+            view.backgroundColor = .grayScale6?.withAlphaComponent(0.5)
+            gramView.delegate = self
+            
+            gramView.snp.makeConstraints {
+                $0.center.equalToSuperview()
+                $0.width.equalTo(327)
+                $0.height.equalTo(205)
+            }
+        } else {
+            view.addSubview(defaultView)
+            view.backgroundColor = .grayScale6?.withAlphaComponent(0.5)
+            defaultView.delegate = self
+            
+            defaultView.snp.makeConstraints {
+                $0.center.equalToSuperview()
+                $0.width.equalTo(327)
+                $0.height.equalTo(205)
+            }
         }
     }
     
     override func viewIsAppearing(_ animated: Bool) {
         super.viewIsAppearing(animated)
-        guard let item else { return }
-        gramView.configure(item)
+        if item.ingredient_type == "PIECES" {
+            gramView.configure(item)
+        } else {
+            defaultView.configure(item)
+        }
     }
 }
 
 extension AddIngredientViewController: AddIngredientCountViewDelegate {
     func didTappedCancelButton() {
-//        gramView.
-        guard let item else { return }
-        print(item.ingredient_id)
-        print(gramView.count)
         dismiss(animated: true)
     }
     
     func didTappedOkButton(_ count: Int) {
-        guard let item else { return }
-        self.forTag = "\(item.ingredient_name) \(gramView.count)개"
+        if item.ingredient_type == "PIECES" {
+            self.forTag = "\(item.ingredient_name) \(gramView.count)개"
+        } else {
+            self.forTag = "\(item.ingredient_name) \(defaultView.count)\(item.ingredient_unit)"
+        }
         didSendEventClosure?(.okButtonTapped)
         dismiss(animated: true)
     }
 }
-
 
