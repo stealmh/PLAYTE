776a4056ea7dcc2d05d11be3e72869ca3ae01bd3
stealmh <kmh922@naver.com>
2023-09-05 12:59:49 +0900

    [fix] #43 다크모드 비활성화

--- Recipe/Application/AppDelegate.swift
+++ Recipe/Application/AppDelegate.swift
@@ -14,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        print(#function)
+        UIApplication.shared.windows.forEach { window in
+            window.overrideUserInterfaceStyle = .light
+        }
         return true
     }
 
