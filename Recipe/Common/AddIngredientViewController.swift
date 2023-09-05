49bfbbcf40d549bc206d4408543595420c84bd44
stealmh <kmh922@naver.com>
2023-09-06 01:05:21 +0900

    [fix] 재료추가 버그 수정

--- Recipe/Common/AddIngredientViewController.swift
+++ Recipe/Common/AddIngredientViewController.swift
@@ -33,7 +33,7 @@ class AddIngredientViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if item.ingredient_type == "INGREDIENTS" {
+        if item.ingredient_unit == "PIECE" {
             view.addSubview(gramView)
             view.backgroundColor = .grayScale6?.withAlphaComponent(0.5)
             gramView.delegate = self
@@ -54,7 +54,7 @@ class AddIngredientViewController: BaseViewController {
                 $0.height.equalTo(205)
             }
         }
-        if item.ingredient_type == "INGREDIENTS" {
+        if item.ingredient_unit == "PIECE" {
             gramView.configure(item)
         } else {
             defaultView.configure(item)
@@ -78,7 +78,7 @@ extension AddIngredientViewController: AddIngredientCountViewDelegate {
     
     func didTappedOkButton(_ count: Int) {
         print(item)
-        if item.ingredient_type == "INGREDIENTS" {
+        if item.ingredient_unit == "PIECE" {
             print("== if 문입니다 ==")
             self.forTag = "\(item.ingredient_name) \(count)개"
             self.ingredient = UploadRecipeIngredient(ingredient_id: item.ingredient_id, ingredient_size: count)
