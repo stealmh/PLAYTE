c243fe58ab69dfec97d330fd7453fac5b798e4e2
stealmh <kmh922@naver.com>
2023-09-07 23:09:15 +0900

    [feat] #45 회원가입 뷰모델 추가

---
+++ Recipe/Scenes/Register/RegisterViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  RegisterViewModel.swift
+//  Recipe
+//
+//  Created by 김민호 on 2023/09/07.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class RegisterViewModel {
+    
+    // Input
+    let nickNameInput = PublishRelay<String>()
+    
+    // Output
+    let isNickNameValid: Driver<(Bool, String)>
+    
+    init() {
+        let nickNameResult = nickNameInput
+            .debounce(.seconds(1), scheduler: MainScheduler.instance)
+            .flatMapLatest { nickName -> Observable<(Bool, String)> in
+                return Observable.create { observer in
+                    LoginService.shared.nickNameCheck(nickName: nickName, completion: { data in
+                        observer.onNext((data, nickName))
+                    })
+                    return Disposables.create()
+                }
+            }
+            .share(replay: 1)
+        
+        isNickNameValid = nickNameResult
+            .asDriver(onErrorJustReturn: (false, ""))
+    }
+    
+    deinit {
+        print("deinit")
+    }
+}
